5a114d2b7efe55af95152aff8cd5edc5c53c2948
gabrielzv1233 <77300296+gabrielzv1233@users.noreply.github.com>
2026-09-06 03:07:26 -0500

    feat: initialize global logging

--- src/GiMateButtonRemapper/Program.cs
+++ src/GiMateButtonRemapper/Program.cs
@@ -6,6 +6,20 @@ internal static class Program
     private static void Main()
     {
         ApplicationConfiguration.Initialize();
+        AppLog.Initialize();
+
+        Application.SetUnhandledExceptionMode(UnhandledExceptionMode.CatchException);
+        Application.ThreadException += (_, e) => AppLog.Exception("Unhandled UI-thread exception", e.Exception);
+        AppDomain.CurrentDomain.UnhandledException += (_, e) =>
+        {
+            if (e.ExceptionObject is Exception ex)
+                AppLog.Exception("Unhandled application exception", ex);
+            else
+                AppLog.Error($"Unhandled non-Exception object: {e.ExceptionObject}");
+        };
+
+        AppLog.Info("Starting main window");
         Application.Run(new MainForm());
+        AppLog.Info("GiHATE exited normally");
     }
 }
